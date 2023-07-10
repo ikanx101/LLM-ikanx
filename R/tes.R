84a25bf222f4206d2e08fbda3853e02badaa5836
ikanx101 <ikanx101@gmail.com>
2023-07-10 10:16:26 +0700

    kita save terlebih dahulu

---
+++ R/tes.R
@@ -0,0 +1,5 @@
+https://blogs.rstudio.com/ai/posts/2017-12-22-word-embeddings-with-keras/
+https://rpubs.com/nabiilahardini/word2vec
+https://cbail.github.io/textasdata/word2vec/rmarkdown/word2vec.html
+https://www.theanalyticslab.nl/nlpblogs_2_training_word_embedding_models_and_visualize_results/
+https://medium.com/cmotions/natural-language-processing-for-predictive-purposes-with-r-cb65f009c12b
